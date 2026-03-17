327b207154b026d0a09b689175da4dea394e9c45
Wither4096 <35404007+Wither4096@users.noreply.github.com>
2026-03-17 03:12:19 +0300

    setup sensor readings

--- uMonitor.ino
+++ uMonitor.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <DHT.h>
+//#define RZERO -10
 
+const int analogMQPin=25;
+const int digitalMQPin=33;
+const int dhtPin=32;
+
+DHT dhtSens(dhtPin,DHT11);
+
+void setup() {
+  Serial.begin(9600);
+  dhtSens.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  float hum=dhtSens.readHumidity();
+  float tempu=dhtSens.readTemperature();
+  float analogMQVal=analogRead(analogMQPin);
+  float digitalMQVal=digitalRead(digitalMQPin);
+  float mappedMQVal=map(analogMQVal,0,4095,0,1023);
+  Serial.println("HUM/TEMP: ");
+  Serial.print("Humidity: ");Serial.println(hum);
+  delay(500);
+  Serial.print("Temperature: ");Serial.println(tempu);
+  delay(500);
+  Serial.print("PPM: ");Serial.println(mappedMQVal);
+  delay(1000);
 }
