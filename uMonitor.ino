79044a6a229c1d335d48d930d06354aa636d462d
Wither4096 <35404007+Wither4096@users.noreply.github.com>
2026-03-19 01:55:12 +0300

    added lm35 for reduendency

--- uMonitor.ino
+++ uMonitor.ino
@@ -1,19 +1,23 @@
 #include <DHT.h>
-//#define RZERO -10
 
 const int analogMQPin=34;
-//const int digitalMQPin=33;
 const int dhtPin=33;
+const int lm35pin=13;
 const int buzzer=15;
 
 DHT dhtSens(dhtPin,DHT11);
 
 float hum=0;
-float tempu=0;
+float tempuDHT=0;
+float tempuLM35=0;
+float mappedLM35Val=0;
 float analogMQVal=0;
 float mappedMQVal=0;
 
 void setup() {
+  pinMode(analogMQPin,INPUT);
+  pinMode(dhtPin,INPUT);
+  pinMode(lm35pin,INPUT);
   pinMode(buzzer,OUTPUT);
   Serial.begin(9600);
   dhtSens.begin();
@@ -21,7 +25,8 @@ void setup() {
 
 void loop() {
   hum=dhtSens.readHumidity();
-  tempu=dhtSens.readTemperature();
+  tempuDHT=dhtSens.readTemperature();
+  tempuLM35=((analogRead(lm35pin))/4095.0)*100.0;
   analogMQVal=analogRead(analogMQPin);
   mappedMQVal=map(analogMQVal,0,4095,0,1023);
   alertAir(mappedMQVal);
@@ -30,8 +35,10 @@ void loop() {
 
 void serialLogDataPoints(){
   Serial.println("---START LOG---");
-  Serial.print("Humidity: ");Serial.println(hum);
-  Serial.print("Temperature: ");Serial.println(tempu);
+  Serial.print("Humidity: ");Serial.print(hum);Serial.println("%");
+  Serial.print("Temperature (DHT11): ");Serial.print(tempuDHT);Serial.println("\xC2\xB0");
+  Serial.print("Temperature (LM35): ");Serial.print(tempuLM35);Serial.println("\xC2\xB0");
+  Serial.print("Temperature (Average): ");Serial.print((tempuDHT+tempuLM35)/2);Serial.println("\xC2\xB0");
   Serial.print("Raw MQ135: ");Serial.println(analogMQVal);
   Serial.print("Mapped MQ135: ");Serial.println(mappedMQVal);
   Serial.println("---END LOG---");
