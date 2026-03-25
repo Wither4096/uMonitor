0b37b4fd4dd54bd76837de02bee208194290c5f6
Wither4096 <35404007+Wither4096@users.noreply.github.com>
2026-03-25 20:05:36 +0300

    logging file spacing for readability

--- Logging.ino
+++ Logging.ino
@@ -1,15 +1,30 @@
+#ifdef LOGGING
 void serialLogDataPoints(){
   Serial.println("---START LOG---");
   
-  Serial.print("Humidity: ");Serial.print(vars.getHum());Serial.println("%");
-  Serial.print("Temperature (DHT11): ");Serial.print(vars.getTempDHT());Serial.println("\xC2\xB0");
-  Serial.print("Temperature (LM35): ");Serial.print(vars.getTempLM35());Serial.println("\xC2\xB0");
-  Serial.print("Temperature (Average): ");Serial.print(vars.getTempAvg());Serial.println("\xC2\xB0");
-  Serial.print("Light Intensity : ");Serial.println(vars.getLight());
-  Serial.print("Raw MQ135: ");Serial.println(vars.getGas());
-  Serial.print("Mapped MQ135: ");Serial.println(map(vars.getGas(),0,4095,0,1023));
+  Serial.print("Humidity: ");Serial.print(readings.getHum());Serial.println("%");
+
+
+  Serial.print("Temperature (DHT11): ");Serial.print(readings.getTempDHT());Serial.println("\xC2\xB0");
+
+
+  Serial.print("Temperature (LM35): ");Serial.print(readings.getTempLM35());Serial.println("\xC2\xB0");
+
+
+  Serial.print("Temperature (Average): ");Serial.print(readings.getTempAvg());Serial.println("\xC2\xB0");
+
+
+  Serial.print("Light Intensity : ");Serial.println(readings.getLight());
+
+
+  Serial.print("Raw MQ135: ");Serial.println(readings.getGas());
+
+
+  Serial.print("Mapped MQ135: ");Serial.println(map(readings.getGas(),0,4095,0,1023));
+
+
   
-  Serial.println("---END LOG---");
-  Serial.println();
+  Serial.println("---END LOG---");Serial.println();
   delay(500);
 };
+#endif
